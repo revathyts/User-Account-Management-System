3d7992bb0966d21416337e151abb0af50cbd3ada
revathyts <79398169+revathyts@users.noreply.github.com>
2021-03-03 18:15:22 +0530

    Add files via upload

--- application/views/forgotpsw.php
+++ application/views/forgotpsw.php
@@ -1,13 +1,38 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Forgot Password</title>
+	<title>Forget Password</title>
+	<title>Registration</title>
+<meta charset=utf-8>
+            <meta name="viewport" content="width=device-width,initial-scale=1">
+
+             <!---Fontawesome--->
+            <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+            <!---Bootstrap5----->
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
 </head>
 <body>
-<form action=<?php echo base_url()?>main/>
-<input type="text" id="uname" name="uname"  placeholder="username" required>
-</br>
-<input type="submit" name="submit" >
+	<h2>Forgot Password</h2><br>
+<?php
+if($error=$this->session->flashdata('msg'))
+{
+?>
+<p style="color:green;">
+<strong>Success</strong>
+<?php
+echo $error;
+?>
+</p>
+<?php
+}
+?>
+<form action="<?php echo base_url(); ?>main/send" method="post">
+    <input type="email" name="from" class="form-control" placeholder="Enter Email" required><br>
+    <textarea name="message" class="form-control" placeholder="Enter message here" required></textarea><br>
+    <button type="submit" class="btn btn-primary">Send Message</button>
 </form>
+
 </body>
 </html>
+	
