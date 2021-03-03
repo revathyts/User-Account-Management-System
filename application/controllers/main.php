cdfc8e617e493846597c3c68b2e8a1dd89748059
revathyts <79398169+revathyts@users.noreply.github.com>
2021-03-03 18:19:38 +0530

    Update main.php

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -248,6 +248,61 @@ public function logout()
         }
     }
     /**********logout end**************/
+	
+/****forgot password****/
+
+public function forget()
+{
+$this->load->view('forgotpsw');
+}
+
+public function send()
+{
+    $to =  $this->input->post('from');  // User email pass here
+    $subject = 'Welcome To Elevenstech';
+
+    $from = 'team3orisys@gmail.com';              // Pass here your mail id
+
+    $emailContent = '<!DOCTYPE><html><head></head><body><table width="600px" style="border:1px solid #cccccc;margin: auto;border-spacing:0;"><tr><td style="background:#000000;padding-left:3%"><img src="http://elevenstechwebtutorials.com/assets/logo/logo.png" width="300px" vspace=10 /></td></tr>';
+    $emailContent .='<tr><td style="height:20px"></td></tr>';
+
+
+    $emailContent .= $this->input->post('message');  //   Post message available here
+
+
+    $emailContent .='<tr><td style="height:20px"></td></tr>';
+    $emailContent .= "<tr><td style='background:#000000;color: #999999;padding: 2%;text-align: center;font-size: 13px;'><p style='margin-top:1px;'><a href='http://elevenstechwebtutorials.com/' target='_blank' style='text-decoration:none;color: #60d2ff;'>www.elevenstechwebtutorials.com</a></p></td></tr></table></body></html>";
+               
+
+
+    $config['protocol']    = 'smtp';
+    $config['smtp_host']    = 'ssl://smtp.gmail.com';
+    $config['smtp_port']    = '465';
+    $config['smtp_timeout'] = '60';
+
+    $config['smtp_user']    = 'team3orisys@gmail.com';    //Important
+    $config['smtp_pass']    = 'karr@orisys';  //Important
+
+    $config['charset']    = 'utf-8';
+    $config['newline']    = "\r\n";
+    $config['mailtype'] = 'html'; // or html
+    $config['validation'] = TRUE; // bool whether to validate email or not
+
+     
+
+    $this->email->initialize($config);
+    $this->email->set_mailtype("html");
+    $this->email->from($from);
+    $this->email->to($to);
+    $this->email->subject($subject);
+    $this->email->message($emailContent);
+    $this->email->send();
+
+    $this->session->set_flashdata('msg',"Mail has been sent successfully");
+    $this->session->set_flashdata('msg_class','alert-success');
+    return redirect('main/forget');
+}
+
 
 /*****email avaliability check *****/
 
@@ -303,6 +358,8 @@ public function uname_availability()
                      echo '<label class="text-success"><span class="glyphicon glyphicon-ok"></span> </label>';  
                 }  
       }
+	
+	
 
 
 }
